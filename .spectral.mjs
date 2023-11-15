ae1f9366d5949a18950cd65845cb277f8b62a8d5
hemantmann <hemant.mann@trackier.com>
2023-11-15 23:13:45 +0530

    fix: open api errors

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIyNjMyMQ/spectral.js?branch_slug=master&token=45cb6344-03ac-4189-b45b-5ae5799063e3";
+export default { extends: ruleset };
